1b5b3006d375092ab220297b9462f7085886d2dc
窦妍 <douyan001@lianjia.com>
2017-12-06 18:49:23 +0800

    add router

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import store from './store'
-import App from './components/App'
+import router from './router'
+// import App from './components/App'
 import { Menu, Submenu, MenuItem, Button } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(Menu)
@@ -8,15 +9,21 @@ Vue.use(Submenu)
 Vue.use(MenuItem)
 Vue.use(Button)
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
 
-new Vue({
-    store, // inject store to all children
-    el: '#app',
-    render: h => h(App)
-})
+// new Vue({
+//     router,
+//     store, // inject store to all children
+//     el: '#app'
+//     // render: h => h(App)
+// })
 // new Vue({
 //   el: '#app',
 //   template: '<App/>',
 //   components: { App }
 // })
+
+const app = new Vue({
+    router,
+    store
+}).$mount('#app')
