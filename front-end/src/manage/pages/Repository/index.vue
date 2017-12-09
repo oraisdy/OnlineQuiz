c35df6e067a1059ae1a6b8985d93d63cbb69a759
oraisdy <569450503@qq.com>
2017-12-09 22:23:58 +0800

    docker file

--- front-end/src/manage/pages/Repository/index.vue
+++ front-end/src/manage/pages/Repository/index.vue
@@ -4,7 +4,7 @@
         <QuestionList/>
         <el-upload class="upload" action="https://jsonplaceholder.typicode.com/posts/" :on-preview="handlePreview" :on-remove="handleRemove" multiple :limit="3" :on-exceed="handleExceed" :file-list="fileList">
             <el-button size="small" type="primary">点击上传</el-button>
-            <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+            <div slot="tip" class="el-upload__tip">只能上传xslx文件，且不超过500kb</div>
         </el-upload>
     </div>
 </template>
