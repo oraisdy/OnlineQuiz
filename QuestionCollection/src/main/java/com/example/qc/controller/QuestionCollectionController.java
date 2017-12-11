6ca6fe4534fcdaf793fe55cd39ce3ffd4c1de233
dym <dingyinmen001@lianjia.com>
2017-12-11 11:15:58 +0800

    complete feature: get question by tag & subject

--- QuestionCollection/src/main/java/com/example/qc/controller/QuestionCollectionController.java
+++ QuestionCollection/src/main/java/com/example/qc/controller/QuestionCollectionController.java
@@ -1,5 +1,7 @@
-package  com.example.qc;
+package com.example.qc.controller;
 
+import com.example.qc.model.Answer;
+import com.example.qc.model.Question;
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.*;
@@ -8,31 +10,22 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
+import java.util.Vector;
 
-
-public class InputQuestionFromExcel {
+public class QuestionCollectionController {
+    private String url="jdbc:mysql://118.89.114.168:3306/online_quiz?autoReconnect=true&useSSL=false&characterEncoding=utf-8";
+    private String user="root";
+    private String password="2a617";
+    private Connection conn;
 
     public static void main(String[] args){
-        InputQuestionFromExcel inputQuestionFromExcel = new InputQuestionFromExcel();
-        try{
-            inputQuestionFromExcel.readTheInputExcel("/Users/dym/Documents/大作业/输入的试题.xlsx");
-
-        }
-        catch (Exception exception){
-            System.out.println(exception);
-        }
+        QuestionCollectionController qc = new QuestionCollectionController();
+        System.out.println(qc.getQuestionsByTagAndSubject("tag","software").size());
     }
 
     public void readTheInputExcel(String path) throws EncryptedDocumentException, InvalidFormatException, IOException {
 
-        String url="jdbc:mysql://localhost:3306/question?autoReconnect=true&useSSL=false&characterEncoding=utf-8";
-        String user="root";
-        String password="123456";
-        Connection conn;
-
-
         InputStream inputStream = new FileInputStream(path);
-        //InputStream inp = new FileInputStream("C:/Users/H__D/Desktop/workbook.xls");
 
         Workbook workbook = WorkbookFactory.create(inputStream);
         Sheet sheet = workbook.getSheetAt(0);
@@ -46,16 +39,18 @@ public class InputQuestionFromExcel {
         }
         try{
             conn.setAutoCommit(false);
-            String sql ="INSERT INTO questions VALUES(NULL,?,?,?)";
-            PreparedStatement pstmt = conn.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            String sql ="INSERT INTO questions VALUES(NULL,?,?,?,?)";
+            PreparedStatement pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             for (Row row : sheet) {
                 String question = formatter.formatCellValue(row.getCell(0));
                 String subject = formatter.formatCellValue(row.getCell(1));
                 String[] trueAnswers = formatter.formatCellValue(row.getCell(2)).split(" ");
+                String tag = formatter.formatCellValue(row.getCell(3));
 
                 pstmt.setString(1,question);
-                pstmt.setString(3,subject);
                 pstmt.setString(2,trueAnswers.length+"");
+                pstmt.setString(3,subject);
+                pstmt.setString(4,tag);
                 pstmt.addBatch();
 
             }
@@ -99,7 +94,50 @@ public class InputQuestionFromExcel {
             e.getNextException();
         }
 
-
     }
 
+    public Vector<Question> getQuestionsByTagAndSubject(String tag, String subject){
+        Vector<Question> result = new Vector<>();
+        try{
+            conn= DriverManager.getConnection(url,user,password);
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+            return result;
+        }
+        try{
+            //建立PreparedStatement对象
+            String sql="select * from questions where Subject=? and Tag=?";
+            PreparedStatement pstmt=conn.prepareStatement(sql);
+            pstmt.setString(1,subject);
+            pstmt.setString(2,tag);
+            ResultSet rs=pstmt.executeQuery();
+            while (rs.next()){
+                int questionId = rs.getInt("ID");
+                sql="select * from answers where QuestionID=?";
+                pstmt=conn.prepareStatement(sql);
+                pstmt.setInt(1,questionId);
+                ResultSet answers = pstmt.executeQuery();
+                Vector<Answer> allAnswers = new Vector<>();
+                while (answers.next()){
+                    Answer answer = new Answer(answers.getInt("ID"),
+                            answers.getString("Content"),
+                            answers.getInt("QuestionID"),
+                            answers.getInt("Type"));
+                    allAnswers.add(answer);
+                }
+                Question question = new Question(questionId,
+                        rs.getString("Content"),
+                        allAnswers,
+                        rs.getInt("AnswerNumber"),
+                        rs.getString("Subject"),
+                        rs.getString("Tag"));
+                result.add(question);
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+            e.getNextException();
+        }
+        return result;
+    }
 }
