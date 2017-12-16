1e9f0cbf2152b954e4d05a1ce55ea46de54f5044
oraisdy <569450503@qq.com>
2017-12-16 18:32:12 +0800

    deploy

--- front-end/src/quiz/config/index.js
+++ front-end/src/quiz/config/index.js
@@ -1,6 +1,6 @@
 export const QS_API_URL = "http://114.115.137.197:3333";
-export const QC_API_URL = "http://114.115.137.197:2222";
-export const JW_API_URL = "http://114.115.137.197:3333";
+export const QC_API_URL = "http://114.115.137.197:4444";
+export const JW_API_URL = "http://114.115.137.197:4444";
 
 // export const QS_API_URL = "http://localhost:3333";
 // export const QC_API_URL = "http://localhost:4444";
