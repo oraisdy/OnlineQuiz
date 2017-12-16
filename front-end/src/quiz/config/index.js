ceef136a1d410f5dc34754e18a79c4f602b11f29
oraisdy <569450503@qq.com>
2017-12-16 18:02:08 +0800

    deploy

--- front-end/src/quiz/config/index.js
+++ front-end/src/quiz/config/index.js
@@ -1,3 +1,7 @@
 export const QS_API_URL = "http://114.115.137.197:3333";
 export const QC_API_URL = "http://114.115.137.197:2222";
 export const JW_API_URL = "http://114.115.137.197:3333";
+
+// export const QS_API_URL = "http://localhost:3333";
+// export const QC_API_URL = "http://localhost:4444";
+// export const JW_API_URL = "http://localhost:4444";
