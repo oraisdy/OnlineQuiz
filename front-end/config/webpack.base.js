2c9f833d059e8b0c548997a73f5562b41cffc19b
oraisdy <569450503@qq.com>
2017-12-20 14:57:55 +0800

    optimise

--- front-end/config/webpack.base.js
+++ front-end/config/webpack.base.js
@@ -1,70 +1,69 @@
-const path = require("path");
-const projectRoot = path.resolve(__dirname, "../");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const webpack = require("webpack");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require('path')
+const webpack = require('webpack')
+const projectRoot = path.resolve(__dirname, '../')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 function resolve(relPath) {
-    return path.resolve(__dirname, relPath);
+    return path.resolve(__dirname, relPath)
 }
 
 module.exports = {
     entry: {
-        app: "./src/quiz/index.js",
-        manage: "./src/manage/index.js"
+        app: './src/quiz/index.js',
+        manage: './src/manage/index.js'
     },
     output: {
-        path: path.resolve(__dirname, "../dist"),
-        // publicPath: path.resolve(__dirname, '../public'),
-        filename: "[name].js"
+        path: path.resolve(__dirname, '../dist'),
+        filename: '[name].[chunkhash].js'
     },
     resolve: {
-        extensions: [".js", ".vue"],
+        extensions: ['.js', '.vue'],
         alias: {
-            vue$: "vue/dist/vue.esm.js",
-            src: path.resolve(__dirname, "../src"),
-            assets: path.resolve(__dirname, "../src/assets"),
-            components: path.resolve(__dirname, "../src/components")
+            vue$: 'vue/dist/vue.esm.js',
+            src: path.resolve(__dirname, '../src'),
+            assets: path.resolve(__dirname, '../src/assets'),
+            components: path.resolve(__dirname, '../src/components')
         }
     },
     module: {
         rules: [
             {
                 test: /\.js$/,
-                include: [resolve("src"), resolve("test")],
+                include: [resolve('src'), resolve('test')],
                 exclude: /node_modules/,
                 use: {
-                    loader: "babel-loader"
+                    loader: 'babel-loader'
                 }
             },
             {
                 test: /\.vue$/,
                 use: {
-                    loader: "vue-loader"
+                    loader: 'vue-loader'
                     // options: utils.vueLoaderOptions()
                 }
             },
             {
                 test: /\.css$/,
                 use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: "css-loader"
+                    fallback: 'style-loader',
+                    use: 'css-loader'
                 })
             },
             {
                 test: /\.less$/,
                 use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: ["css-loader", "less-loader"]
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'less-loader']
                 })
             },
             {
                 test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
                 use: {
-                    loader: "url-loader",
+                    loader: 'url-loader',
                     options: {
                         limit: 10000,
-                        name: "images/[name].[hash:7].[ext]"
+                        name: 'images/[name].[hash:7].[ext]'
                     }
                 }
             },
@@ -72,10 +71,10 @@ module.exports = {
                 test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
                 use: [
                     {
-                        loader: "url-loader",
+                        loader: 'url-loader',
                         options: {
                             limit: 10000,
-                            name: "fonts/[name].[hash:7].[ext]"
+                            name: 'fonts/[name].[hash:7].[ext]'
                         }
                     }
                 ]
@@ -83,28 +82,48 @@ module.exports = {
         ]
     },
     plugins: [
-        // new HtmlWebpackPlugin({
-        //     filename: 'index.html',
-        //     template: 'index.html',
-        //     inject: true
-        // }),
-        new ExtractTextPlugin("style.css"),
-        new HtmlWebpackPlugin({
-            inject: true,
-            chunks: ["app"],
-            template: "index.html",
-            filename: "app.html"
-        }),
-        new HtmlWebpackPlugin({
-            inject: true,
-            chunks: ["manage"],
-            template: "index.html",
-            filename: "manage.html"
-        }),
+        new ExtractTextPlugin('style.css'),
         new webpack.DefinePlugin({
-            "process.env.NODE_ENV": JSON.stringify(
-                process.env.NODE_ENV || "development"
+            'process.env.NODE_ENV': JSON.stringify(
+                process.env.NODE_ENV || 'development'
             )
+        }),
+        /* decouple your vendor code from your application code */
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: function(module) {
+                // This prevents stylesheet resources with the .css or .scss extension
+                // from being moved from their original chunk to the vendor chunk
+                if (
+                    module.resource &&
+                    /^.*\.(css|less)$/.test(module.resource)
+                ) {
+                    return false
+                }
+                return (
+                    module.context &&
+                    module.context.indexOf('node_modules') !== -1
+                )
+            }
+        }),
+        new webpack.HashedModuleIdsPlugin(),
+        new HtmlWebpackPlugin({
+            // files: {
+            //     css: ['style.css']
+            // },
+            inject: true,
+            chunks: ['app', 'vendor'],
+            template: 'index.html',
+            filename: 'app.html'
+        }),
+        new HtmlWebpackPlugin({
+            // files: {
+            //     css: ['style.css']
+            // },
+            inject: true,
+            chunks: ['manage', 'vendor'],
+            template: 'index.html',
+            filename: 'manage.html'
         })
     ]
-};
+}
--- front-end/manage-server/manage.js
+++ front-end/manage-server/manage.js
@@ -1,22 +1,24 @@
-const Koa = require("koa");
-const morgan = require("koa-morgan");
-const path = require("path");
-const static = require("koa-static");
-const fs = require("fs");
-const manage = new Koa();
+const Koa = require('koa')
+const morgan = require('koa-morgan')
+const path = require('path')
+const static = require('koa-static')
+const fs = require('fs')
+const manage = new Koa()
+
+app.enableCache = true
 // logger
 manage.use(
     morgan(
         ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'
     )
-);
+)
 // static assets
-manage.use(static(path.resolve(__dirname, "..", "dist")));
+manage.use(static(path.resolve(__dirname, '..', 'dist')))
 //异步读取文件的形式
 manage.use(async (ctx, next) => {
-    ctx.type = "html";
+    ctx.type = 'html'
     ctx.body = await fs.createReadStream(
-        path.resolve(__dirname, "..", "dist", "manage.html")
-    );
-});
-module.exports = manage;
+        path.resolve(__dirname, '..', 'dist', 'manage.html')
+    )
+})
+module.exports = manage
